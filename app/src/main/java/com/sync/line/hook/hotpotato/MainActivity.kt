e295115f1ed638f0ffff9dfdeb2c0a7476603eaa
MJ Thornton <you@example.com>
2018-08-20 13:34:38 -0700

    Added a blinking and bouncing animation

--- app/src/main/java/com/sync/line/hook/hotpotato/MainActivity.kt
+++ app/src/main/java/com/sync/line/hook/hotpotato/MainActivity.kt
@@ -20,11 +20,12 @@ import android.support.constraint.ConstraintLayout
 class MainActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val timer = MyCounter(4000, 250)
+        val timer = MyCounter(16000, 250)
 
         var btn_start = findViewById(R.id.btn_start) as ImageButton
         var tv_countdown = findViewById(R.id.tv_countdown) as TextView
@@ -32,44 +33,71 @@ class MainActivity : AppCompatActivity() {
 
         btn_start.setOnClickListener { view ->
             timer.start()
+
             tv_countdown.visibility = VISIBLE
-
-            val bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce)
-            view.startAnimation(bounceAnimation)
-            manageBlinkEffect()
-
             btn_start.setEnabled(false)
         }
 
     }
 
+    // Blink red and white on each countdown increment
     @SuppressLint("WrongConstant")
-    fun manageBlinkEffect(){
+    fun manageBlinkEffect(duration: Long, start : Boolean){
         val anim = ObjectAnimator.ofInt(cl_container, "backgroundColor", Color.WHITE, Color.RED,
                 Color.WHITE)
-        anim.duration = 1500
-        anim.setEvaluator(ArgbEvaluator())
-        anim.repeatMode = Animation.REVERSE
-        anim.repeatCount = Animation.INFINITE
-        anim.start()
+        if(start == true) {
+            anim.duration = duration
+            anim.setEvaluator(ArgbEvaluator())
+            anim.repeatMode = Animation.REVERSE
+            anim.repeatCount = Animation.INFINITE
+            anim.start()
+        }else{
+            anim.end()
+        }
     }
 
     inner class MyCounter(millisInFuture: Long, countDownInterval: Long) : CountDownTimer(millisInFuture, countDownInterval) {
 
-        override fun onFinish() {
-            println("Timer complete")
-            tv_countdown.visibility = INVISIBLE;
-        }
+        // This the the start timer, once the user clicks the "Go Button"
+        // 3..2..1..Go!
+        var timeLeft : Long = millisInFuture - 3000
+
+        var blinkAnimation_Duration : Long = 1500
+
+
 
         override fun onTick(millisUntilFinished: Long) {
-            if(millisUntilFinished.compareTo(1000) == 1){
-                tv_countdown.text = (millisUntilFinished / 1000).toString()
+
+            if(millisUntilFinished > timeLeft){
+                tv_countdown.text = ((millisUntilFinished - timeLeft + 1000) / 1000).toString()
             }else{
-                tv_countdown.text = ("GO!")
+
+                if(millisUntilFinished <= timeLeft && millisUntilFinished > timeLeft - 250 ) {
+                    tv_countdown.text = ("GO!")
+                    println("GO!")
+                    val bounceAnimation = AnimationUtils.loadAnimation(applicationContext, R.anim.bounce)
+                    btn_start.startAnimation(bounceAnimation)
+                    manageBlinkEffect(blinkAnimation_Duration, true)
+
+                }else{
+                    blinkAnimation_Duration = millisUntilFinished / 2
+                    println("Start Countdown done!")
+                    tv_countdown.visibility = INVISIBLE
+                    manageBlinkEffect(blinkAnimation_Duration, true)
+
+                    }
             }
 
-            println("Timer : " + millisUntilFinished / 1000)
+            println("Timer : " + millisUntilFinished / 1000 + " Time Left: $timeLeft")
         }
+
+        override fun onFinish() {
+            println("Timer complete")
+            manageBlinkEffect(blinkAnimation_Duration, false)
+        }
+
+
+
     }
 
 }
